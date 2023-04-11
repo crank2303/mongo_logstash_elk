9f4204fbd84d14b99461b6ecdb022add95fd6810
Alexey <aa.reukov@gmail.com>
2023-04-12 06:28:22 +1100

    Added research

--- ugc_mongo/tests/functional/settings.py
+++ ugc_mongo/tests/functional/settings.py
@@ -1,27 +1,15 @@
-import os
-
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class TestSettings(BaseSettings):
-    mongo_host: str
-    mongo_port: int
-    redis_host: str
-    redis_port: int
-    service_protocol: str
-    service_host: str
-    service_port: str
-    service_api_version: int
+    mongo_host: str = Field(..., env="MONGO_HOST") 
+    mongo_port: int = Field(..., env="MONGO_PORT")
+    redis_host: str = Field(..., env="REDIS_HOST")
+    redis_port: int = Field(..., env="REDIS_PORT")
+    service_protocol: str = Field(..., env="SERVICE_PROTOCOL")
+    service_host: str = Field(..., env="SERVICE_HOST")
+    service_port: str = Field(..., env="SERVICE_PORT")
+    service_api_version: int = Field(..., env="SERVICE_API_VERSION")
 
 
-test_settings = {
-    "mongo_host": os.getenv("MONGO_HOST"),
-    "mongo_port": os.getenv("MONGO_PORT"),
-    "redis_host": os.getenv("REDIS_HOST"),
-    "redis_port": os.getenv("REDIS_PORT"),
-    "service_protocol": os.getenv("SERVICE_PROTOCOL"),
-    "service_host": os.getenv("SERVICE_HOST"),
-    "service_port": os.getenv("SERVICE_PORT"),
-    "service_api_version": os.getenv("SERVICE_API_VERSION"),
-}
-config = TestSettings.parse_obj(test_settings)
+config = TestSettings()
