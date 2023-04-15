28db1c87c8d0324234f9efe266c92a11227abf49
artem <crank2303@gmail.com>
2023-04-16 00:23:31 +0300

    fin

--- ugc_mongo/tests/functional/settings.py
+++ ugc_mongo/tests/functional/settings.py
@@ -2,14 +2,14 @@ from pydantic import BaseSettings, Field
 
 
 class TestSettings(BaseSettings):
-    mongo_host: str = Field(..., env="MONGO_HOST") 
-    mongo_port: int = Field(..., env="MONGO_PORT")
-    redis_host: str = Field(..., env="REDIS_HOST")
-    redis_port: int = Field(..., env="REDIS_PORT")
-    service_protocol: str = Field(..., env="SERVICE_PROTOCOL")
-    service_host: str = Field(..., env="SERVICE_HOST")
-    service_port: str = Field(..., env="SERVICE_PORT")
-    service_api_version: int = Field(..., env="SERVICE_API_VERSION")
+    mongo_host: str = Field("mongors1n1", env="MONGO_HOST")
+    mongo_port: int = Field(27017, env="MONGO_PORT")
+    redis_host: str = Field("cs_redis", env="REDIS_HOST")
+    redis_port: int = Field(6379, env="REDIS_PORT")
+    service_protocol: str = Field("http", env="SERVICE_PROTOCOL")
+    service_host: str = Field("cs_fastapi", env="SERVICE_HOST")
+    service_port: str = Field(8000, env="SERVICE_PORT")
+    service_api_version: int = Field(1, env="SERVICE_API_VERSION")
 
 
 config = TestSettings()
